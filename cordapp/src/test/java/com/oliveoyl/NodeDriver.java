0a132bd29969dd6e19ccf6fdc71d800a8c29e811
joeldudleyr3 <joel.dudley@r3.com>
2018-07-09 20:11:05 +0100

    Removes comment.

--- cordapp/src/test/java/com/oliveoyl/NodeDriver.java
+++ cordapp/src/test/java/com/oliveoyl/NodeDriver.java
@@ -11,20 +11,6 @@ import com.google.common.collect.ImmutableSet;
 
 import static net.corda.testing.driver.Driver.driver;
 
-/**
- * This file is exclusively for being able to run your nodes through an IDE (as opposed to using deployNodes)
- * Do not use in a production environment.
- * <p>
- * To debug your CorDapp:
- * <p>
- * 1. Firstly, run the "Run Template CorDapp" run configuration.
- * 2. Wait for all the nodes to start.
- * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
- * 5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
- * the console output to be sure.
- * 4. Set your breakpoints in your CorDapp code.
- * 5. Run the "Debug CorDapp" remote debug run configuration.
- */
 public class NodeDriver {
     public static void main(String[] args) {
         // No permissions required as we are not invoking flows.
